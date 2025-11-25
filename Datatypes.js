5f429da60173bf37198ed3e69123e6c3a4d06863
PackialakshmiN <laks12feb@gmail.com>
2025-11-25 15:00:44 +0530

    Add files via upload
    week1

---
+++ Datatypes.js
@@ -0,0 +1,20 @@
+var username
+console.log(username); // undefined 
+console.log(typeof username); // "undefined"
+
+var age = 25;
+console.log(age); // 25
+console.log(typeof age); // "number"
+
+var isStudent = true;
+console.log(isStudent); // true
+console.log(typeof isStudent); // "boolean"
+
+var name = "Packia";
+console.log(name); // "Packia"
+console.log(typeof name); // "string"
+
+var marks=null;
+console.log(marks); // null
+console.log(typeof marks); // "object"
+
